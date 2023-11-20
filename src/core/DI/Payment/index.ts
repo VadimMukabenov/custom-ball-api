a78320b5c1d7aaf8c0b36d1c438b317ab469c5f5
vadim <vadim.mukabenov98@gmail.com>
2023-11-20 22:40:37 +0300

    fix

--- src/core/DI/Payment/index.ts
+++ src/core/DI/Payment/index.ts
@@ -2,11 +2,15 @@ import type { Config } from "../../../config";
 import { getYooKassaClient } from "../../../libs/yookassa";
 import PaymentController from "../../controllers/PaymentController";
 import PaymentService from "../../services/PaymentService";
+import UploadService from "../../services/UploadService";
+import { getS3Client } from "../../../libs/s3Client";
 
 export function buildPaymentController(config: Config) {
     const yookassaClient = getYooKassaClient(config);
 
-    const paymentService = new PaymentService(yookassaClient);
+    const s3Client = getS3Client(config);
+    const uploadService = new UploadService(s3Client);
+    const paymentService = new PaymentService(yookassaClient, uploadService);
     const paymentController = new PaymentController(paymentService);
 
     return paymentController;
